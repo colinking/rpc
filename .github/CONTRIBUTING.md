eab89be08bfc81b0b65d0307b6390de6f5f873c5
Colin <me@colinking.co>
2022-08-14 18:21:36 -0400

    ignore unknown fields

--- .github/CONTRIBUTING.md
+++ .github/CONTRIBUTING.md
@@ -2,13 +2,15 @@
 
 ## TODOs
 
-- Ignore unknown fields automatically
 - Review patch semantics
 - Add error types for returning status codes
 - Generate a TS client
 - Introspection API
 - CLI distribution
 - Generate a Go client
+- Support default values from the schema
+- Provide validation errors for schema
+- Inline validation for editing JSON5+JTD files
 
 ### Schema
 
@@ -29,10 +31,14 @@
 
 - Response header with warnings, e.g. unknown field names
 - Encode warnings in error responses
-- Support url-form-encoded
+- Support url-form-encoded: https://brandur.org/fragments/application-x-wwww-form-urlencoded
 - Generate a TS server
 - Support streaming via SSE
 - OpenAPI integration, e.g. Postman
 - Built-in pagination support
 - https://brandur.org/idempotency-keys / https://brandur.org/fragments/is-transient / https://brandur.org/fragments/idempotency-keys-crunchy
 - Linting (e.g. "REST"-like, consistent casing, ...)
+- Basic doc generator
+- Review https://brandur.org/fragments/openai-api - OpenAI, Stripe, Crunchy
+  - https://brandur.org/fragments
+- Encode real examples/
--- pkg/schema/discover.go
+++ pkg/schema/discover.go
@@ -124,11 +124,50 @@ func Discover(root string) (API, error) {
 		if err := json5.Unmarshal(contents, &file); err != nil {
 			return API{}, fmt.Errorf("unmarshaling endpoint file (%q): %w", path, err)
 		}
-		endpoint.Request = file.Request
-		endpoint.Response = file.Response
+
+		endpoint.Request = transformSchema(file.Request)
+		endpoint.Response = transformSchema(file.Response)
 
 		api.Endpoints = append(api.Endpoints, endpoint)
 	}
 
 	return api, nil
+}
+
+func transformSchema(schema jtd.Schema) jtd.Schema {
+	// Perform transformations
+	switch schema.Form() {
+	case jtd.FormProperties:
+		// For backwards/forwards compatibility, always ignore unknown properties.
+		schema.AdditionalProperties = true
+	}
+
+	// Recursively transform any sub-schemas
+	for k, v := range schema.Definitions {
+		schema.Definitions[k] = transformSchema(v)
+	}
+
+	switch schema.Form() {
+	case jtd.FormEmpty, jtd.FormRef, jtd.FormType, jtd.FormEnum:
+		// no-op
+	case jtd.FormElements:
+		*schema.Elements = transformSchema(*schema.Elements)
+	case jtd.FormProperties:
+		for k, v := range schema.Properties {
+			schema.Properties[k] = transformSchema(v)
+		}
+		for k, v := range schema.OptionalProperties {
+			schema.OptionalProperties[k] = transformSchema(v)
+		}
+	case jtd.FormValues:
+		*schema.Values = transformSchema(*schema.Values)
+	case jtd.FormDiscriminator:
+		for k, v := range schema.Mapping {
+			schema.Mapping[k] = transformSchema(v)
+		}
+	default:
+		panic(fmt.Errorf("unexpected schema form: %s", schema.Form()))
+	}
+
+	return schema
 }
